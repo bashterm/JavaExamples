4aaa6c212934e7e99ff4e7c3dd53b505f386daa9
Aidan Sciortino <2001flyingpigs14@gmail.com>
2017-09-20 20:02:03 -0400

    Added Comments

--- QuestionPrompter/src/QuestionPrompter.java
+++ QuestionPrompter/src/QuestionPrompter.java
@@ -3,9 +3,9 @@ public class QuestionPrompter {
 	public static void main(String[] args) { //Main Method
 		Scanner in = new Scanner(System.in); //Initialize a new scanner using the Standard Input
 		
-		System.out.print("Hello, what is your name? ");
-		String name = in.nextLine();
+		System.out.print("Hello, what is your name? "); //Print out a question prompt
+		String name = in.nextLine(); //Read the text until the user presses enter, then store the read text in a String
 		
-		System.out.println("\nHello " + name + ", I'm pleased to meet you.");
+		System.out.println("\nHello " + name + ", I'm pleased to meet you."); //Print back the string plus some extra stuff
 	}
 }
